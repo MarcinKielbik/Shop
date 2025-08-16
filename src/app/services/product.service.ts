65bfb64fc04ef5fc6b6cf0a50ab71bad514f116d
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-08-16 19:01:46 +0200

    feat: create ProductService

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../interfaces/product';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductServiceService {
+export class ProductService {
+
+
+  private products: Product[] = [
+    {id: 1, title: 'Laptop', price: 3800},
+    {id: 1, title: 'Phone', price: 2900},
+    {id: 1, title: 'Headphones', price: 230},
+  ];
 
   constructor() { }
+
+  getProducts(): Observable<Product[]> {
+    return of(this.products);
+  }
+
 }
