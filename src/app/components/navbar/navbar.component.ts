62e289976943474437ddb555c9435f6ecb86f3e6
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-08-23 20:31:22 +0200

    feat(navbar, routing): implement navigation to cart and add routing for products

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { map, Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
- styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+
 
   cartCount$: Observable<number>;
   //cartCount$: Observable<number>;
 
-  constructor(private cartService: CartService) {
+  constructor(private cartService: CartService, private router: Router) {
     this.cartCount$ = this.cartService.cart$.pipe(
       map(items => items.reduce((sum, item) => sum + item.quantity, 0))
     );
 
   }
-
+  
+  goToCart() {
+    this.router.navigate(['/cart']);
+  }
 
 }
